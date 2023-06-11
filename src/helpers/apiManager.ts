2f05e7b09a7ed78fbfe0ddd639ecde3580ee980d
Slava <kruchkovenko@gmail.com>
2023-06-11 23:24:04 +0500

    add config

--- src/helpers/apiManager.ts
+++ src/helpers/apiManager.ts
@@ -11,14 +11,14 @@ class ApiManager {
         return axios.post(apiUrl + "api/Auth/login", {
             login: login,
             password: password
-        })
+        },this.getDefaultConfig())
     }
     register(user:User,password: string){
         return axios.post(apiUrl + "api/Auth/reg", {
             name : user.Name,
             login: user.Login,
             password: password
-        })
+        },this.getDefaultConfig())
     }
     getAllChats(userId:string,token:string){
         return axios.get(apiUrl + `api/User/${userId}/chats`,this.getConfig(token))
@@ -27,6 +27,14 @@ class ApiManager {
         return {
             headers: {
                 Authorization: "Bearer " + token,
+                'Content-Type': 'application/json;charset=UTF-8',
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+    }
+    getDefaultConfig() {
+        return {
+            headers: {
                 'Content-Type': 'application/json;charset=UTF-8',
                 'Access-Control-Allow-Origin': '*',
             }
